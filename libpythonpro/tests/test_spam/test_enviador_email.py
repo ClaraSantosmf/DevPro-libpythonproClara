579350c7feace0110319335b926f5946a73edccb
Clarasantosmf <clarasantosmf@gmail.com>
2022-01-20 21:54:05 -0300

    Uploading news tests.

--- libpythonpro/tests/test_spam/test_enviador_email.py
+++ libpythonpro/tests/test_spam/test_enviador_email.py
@@ -1,4 +1,6 @@
-from libpythonpro.spam.enviador_email import Enviador
+from libpythonpro.spam.enviador_email import Enviador, EmailInvalido
+
+import pytest
 
 
 def test_criar_enviador_de_email():
@@ -6,11 +8,24 @@ def test_criar_enviador_de_email():
     assert enviador is not None
 
 
-def test_remetente():
+@pytest.mark.parametrize('remetente',
+                         ['clarasantosmf@gmail.com', 'foo@bar.com.br'])
+
+def test_remetente(remetente):
     enviador = Enviador()
     resultado = enviador.enviar(
+        remetente,
         'clarasantosmf@gmail.com',
-        'mauricioma@usp.br',
         'arquivamento de procedimento',
         'Desejamos consultar se você quer descartar seus procedimentos')
-    assert 'clarasantosmf@gmail.com' in resultado
+    assert remetente in resultado
+
+@pytest.mark.parametrize('remetente',
+                         ['', 'foo'])
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(remetente,
+            'clarasantosmf@gmail.com',
+            'arquivamento de procedimento',
+            'Desejamos consultar se você quer descartar seus procedimentos')
